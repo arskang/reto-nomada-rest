7041503675cf456367e9c755b053c8c05413e4c4
Edder Silva <arskang@gmail.com>
2021-05-08 13:31:30 -0500

    Documentación apib

--- api/middlewares/index.js
+++ api/middlewares/index.js
@@ -5,7 +5,7 @@ const getDecodedToken = (req, res, next) => {
     const { token } = req;
     jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
         if(error) {
-            res.status(401).json(setResponseHelper({ message: error.message }));
+            res.status(401).json(setResponseHelper({ message: "El token no es correcto o ya ha caducado" }));
         } else {
             req.usuario = decoded;
             next();
